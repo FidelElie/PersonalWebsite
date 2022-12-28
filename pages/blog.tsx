45348d03f9cc3332cf699113a44130f03c0bdfc4
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2022-12-28 17:06:09 +0000

    home and blog page skeletons

---
+++ pages/blog.tsx
@@ -0,0 +1,11 @@
+import { Page, Icon, Navbar } from "@/components/core";
+
+const Blog = () => {
+	return (
+		<Page>
+			<Navbar/>
+		</Page>
+	)
+}
+
+export default Blog;
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+
+import { Page, Navbar } from "@/components/core";
+
+const HomePage: NextPage = () => {
+	return (
+		<Page>
+			<Navbar />
+
+		</Page>
+	)
+}
+
+export default HomePage;
