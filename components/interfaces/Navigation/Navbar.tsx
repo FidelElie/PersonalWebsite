992df3e2ee80a2a1812a865811414078a6acf5fb
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2023-02-15 11:59:07 +0000

    Navbar tweaks

--- components/interfaces/Navigation/Navbar.tsx
+++ components/interfaces/Navigation/Navbar.tsx
@@ -1,33 +1,31 @@
+import { useState } from "react";
+
 import { joinClasses } from "@/library/utilities";
 import { useSupabaseContext, useThemeContext } from "@/library/providers";
 
-import { Link, Box, Container, Show, Flex } from "@/components/core";
+import { Link, Box, Container, Flex, Icon, Button } from "@/components/core";
 
 import { Rocket } from "../Assets/Rocket";
 
 export const Navbar = (props: NavbarProps) => {
 	const { className } = props;
 
+	const [isOpen, setIsOpen] = useState(false);
+
 	const { user } = useSupabaseContext();
 	const { theme, setTheme } = useThemeContext();
 
 	return (
-		<Box className={joinClasses(className)}>
-			<Container className="max-w-7xl py-2 mx-auto flex items-center justify-between">
-				<Flex className="items-center">
+		<Box as="nav" className={joinClasses(isOpen && "bg-gray", className)}>
+			<Container className="max-w-7xl py-3 px-5 mx-auto flex items-center justify-between md:px-0">
+				<Flex className="items-center w-full justify-between">
+					<Button onClick={() => setIsOpen(openState => !openState)}>
+						<Icon name={isOpen ? "close" : "menu"} className="text-gray text-xl dark:text-white"/>
+					</Button>
 					<Link.Wrapper href="/" className="flex items-center mr-2">
 						<Rocket className="text-5xl"/>
 					</Link.Wrapper>
-					<Flex className="space-x-3">
-						<Link href="/music">Music</Link>
-						<Link href="/posts">Posts</Link>
-						<Link href="/about">About</Link>
-						<Show when={!user}>
-							<Link href="/auth">
-								Account
-							</Link>
-						</Show>
-					</Flex>
+					<Icon name="search-2" type="line" className="text-gray text-xl dark:text-white" />
 				</Flex>
 			</Container>
 		</Box>
