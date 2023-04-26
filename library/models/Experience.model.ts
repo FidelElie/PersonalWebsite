3d44c1b57d6559c5ffb70b1486ddac23c63f4fa5
Fidel Elie <38261325+FidelElie@users.noreply.github.com>
2023-04-26 10:17:11 +0100

    Removed tags from schemas

--- library/models/Experience.model.ts
+++ library/models/Experience.model.ts
@@ -2,16 +2,13 @@ import { z } from "zod";
 
 import { registerModel, timestamp } from "@/configs/firebase";
 
-import { TagSchema } from "./Tag.model";
-
 export const ExperienceSchema = z.object({
 	title: z.string(),
 	position: z.string(),
 	description: z.string(),
 	link: z.string().optional(),
 	startDate: timestamp(),
-	endDate: timestamp().nullable().optional(),
-	tags: z.array(z.union([z.string(), TagSchema]))
+	endDate: timestamp().nullable().optional()
 });
 
 export type ExperienceSchema = z.infer<typeof ExperienceSchema>;
--- library/models/Project.model.ts
+++ library/models/Project.model.ts
@@ -1,15 +1,12 @@
 import { z } from "zod";
 
-import { TagSchema } from "./Tag.model";
-
 import { registerModel } from "@/configs/firebase";
 
 export const ProjectSchema = z.object({
 	title: z.string(),
 	description: z.string(),
 	link: z.string().optional(),
-	repo: z.string().optional(),
-	tags: z.array(z.union([z.string(), TagSchema]))
+	repo: z.string().optional()
 });
 
 export type ProjectSchema = z.infer<typeof ProjectSchema>;
